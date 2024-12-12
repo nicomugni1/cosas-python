5187f24ac9abf37db0a7fcc6ec5ec5fe9fc792bc
Nicolás Mugni <nicomugni8@gmail.com>
2024-12-12 15:33:44 -0300

    input numeros actualizado

--- Curso_De_Python/inputs/input_numeros.py
+++ Curso_De_Python/inputs/input_numeros.py
@@ -1,5 +1,5 @@
 #pedirle un nùmero al usuario
-numero = input("dame un numero, capitan: ")
+numero = int(input("dame un numero capitan: "))
 
 #convierto el nùmero a entero y lo multiplico por 2
 #resultado_entero = int(numero) * 2
@@ -9,3 +9,4 @@ resultado_flotante = float(numero) * 2
 
 #mostrando el resultado
 print(resultado_flotante)
+print(numero)
